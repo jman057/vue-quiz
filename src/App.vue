e2f3a841f45b8dcf8bc14503cbd91ff933726411
jman057 <jaymannion@gmail.com>
2019-10-02 21:56:17 +1300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="app">
+    <Header />
+
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="6" offset="3">
+          <QuestionBox
+          v-if="questions.length"
+          v-bind:currentQuestion="questions[index]"
+          v-bind:next="next"
+          />
+        </b-col>
+      </b-row>
+    </b-container>
+
+
+
+
+
+  </div>
+</template>
+
+<script>
+
+import Header from './components/Header.vue'
+import QuestionBox from './components/QuestionBox.vue'
+
+export default {
+  name: 'app',
+  components: {
+
+    Header,
+    QuestionBox
+  },
+  data(){
+    return {
+      questions: [],
+      index: 0
+    }
+  },
+  methods:{
+    next(){
+      this.index++
+      console.log("next function called")
+    }
+  },
+  mounted:function() {
+    fetch('https://opentdb.com/api.php?amount=10&category=27&type=multiple', {
+      method: 'get'
+    })
+    .then((response) => {return response.json()
+    })
+    .then((jsonData) => {
+      this.questions = jsonData.results
+    })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="">
+    <b-nav tabs>
+      <b-nav-item disabled><b>Fancy Quiz App</b></b-nav-item>
+      <b-nav-item disabled>Counter: 4/16</b-nav-item>
+
+    </b-nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
